0016fd113958d9237454137207bde136be148fa4
Georgi200031 <77017548+Georgi200031@users.noreply.github.com>
2022-05-01 16:33:13 +0300

    Update camera.h

--- homework6/camera.h
+++ homework6/camera.h
@@ -16,7 +16,9 @@ class Camera
 		void tilt(double angle);
 		void roll(double angle);	
 };
-void Camera::orientation(double a1,a2,a3,a4,a5,a6,a7,a8,a9)
+void Camera::orientation(double a1,double a2,double a3,
+			 double a4,double a5,double a6,
+			 double a7,double a8,double a9)
 {
 	Matrix mat = Matrix(a1,a2,a3,a4,a5,a6,a7,a8,a9);
 	Ray = Mat.operator*(Ray);
